6982ae67a779998486f4b75e565894bf667a434f
Faraj Farook <farajfarook@gmail.com>
2018-01-10 21:43:56 +1100

    Adding search UI logic

--- AppscoreAncestry.Web/ClientApp/app/components/result-grid/result-grid.component.ts
+++ AppscoreAncestry.Web/ClientApp/app/components/result-grid/result-grid.component.ts
@@ -8,9 +8,25 @@ import { PersonViewModel } from '../../models/PersonViewModel';
 export class ResultGridComponent implements OnInit {
 
     results: PersonViewModel[];
+    pages: number[];
+    currentPage: number;
 
     ngOnInit(): void {
         this.results = [];
         this.results.push(new PersonViewModel(1, "Faraj", "Male", "Kandy"));
+        this.pages = new Array(5);
+        this.currentPage = 2;
+    }
+    
+    paginationEnabled(): boolean {
+        return this.pages.length > 0;
+    }
+     
+    isPageActive(pageIndex: number): boolean{
+        return this.currentPage == (pageIndex + 1);
+    }
+    
+    getPageText(pageIndex: number): string {
+        return (pageIndex + 1).toString();
     }
 }
